a8229960df58f1af756c501579468582511cc2e6
syairaarif <putrisyafira4444@gmail.com>
2024-05-02 12:45:32 +0700

    Add project files.

---
+++ Double_linked_list/Double_linked_list.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    int noMhs;
+    string name;
+    Node *next;
+    Node * prev;
+};
+
+Node* START = NULL;
+
+
+int main()
+{
+    
+}
+
