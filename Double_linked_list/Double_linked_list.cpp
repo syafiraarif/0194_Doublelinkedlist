b6b2408ea42159d6119d378da6e558b4a7ee254d
syairaarif <putrisyafira4444@gmail.com>
2024-05-02 14:22:06 +0700

    membuat fungsi listempety

--- Double_linked_list/Double_linked_list.cpp
+++ Double_linked_list/Double_linked_list.cpp
@@ -137,6 +137,11 @@ void deleteNode()
     delete current;
     cout << "\x1b[32mRecord with roll number " << rollNo << "deleted\x1b[0m" << endl;
 }
+
+bool listempety()
+{
+
+}
 int main()
 {
     
