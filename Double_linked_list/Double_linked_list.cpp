58d1c039548583256d9d6e7c4ed5d9b110453602
syairaarif <putrisyafira4444@gmail.com>
2024-05-02 12:59:31 +0700

    langkah 2-4 dalam menambahkan node

--- Double_linked_list/Double_linked_list.cpp
+++ Double_linked_list/Double_linked_list.cpp
@@ -21,6 +21,15 @@ void addNode()
     cin >> newNode->noMhs;
     cout << "\n Enter the name of the student: ";
     cin >> newNode->name;
+
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    {
+        if (START == NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+    }
 }
 int main()
 {
